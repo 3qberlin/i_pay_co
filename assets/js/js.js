18d1b1b07336c6516d2ae24970893286231fd4ec
Berlin <3q.berlin@gmail.com>
2023-08-27 11:30:30 +0800

    build nav/banner/main/footer

---
+++ assets/js/js.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(".carousel-indicators .btn-carouselWeb:first-child").addClass(
+    "btn-carouselWebActive"
+  );
+
+  $("#carouselWeb").on("slide.bs.carousel", function (event) {
+    $(".carousel-indicators .btn-carouselWeb").removeClass(
+      "btn-carouselWebActive"
+    );
+    $(
+      '.carousel-indicators .btn-carouselWeb[data-bs-slide-to="' +
+        event.to +
+        '"]'
+    ).addClass("btn-carouselWebActive");
+  });
+
+  $(".carousel-indicators .btn-carouselMob:first-child").addClass(
+    "btn-carouselMobActive"
+  );
+
+  $("#carouselMobile").on("slide.bs.carousel", function (event) {
+    $(".carousel-indicators .btn-carouselMob").removeClass(
+      "btn-carouselMobActive"
+    );
+    $(
+      '.carousel-indicators .btn-carouselMob[data-bs-slide-to="' +
+        event.to +
+        '"]'
+    ).addClass("btn-carouselMobActive");
+  });
+});
